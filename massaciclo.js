c6592a18b5b580ec719eb31e2c3edeb5c6831631
Henriquelino17 <90516324+Henriquelino17@users.noreply.github.com>
2022-05-08 13:44:58 -0300

    masaciclo

---
+++ massaciclo.js
@@ -0,0 +1,8 @@
+function Massa_Por_Ciclo() {
+    var N_ciclo = document.getElementById("T_ciclo").value; 
+    var M_ciclo =
+  document.getElementById("Massa_ciclo").value;      
+  var Massa_ciclo = M_ciclo / N_ciclo ;
+  var Massa_Ciclo = Massa_ciclo.toFixed(2); 
+  var elemento = document.getElementById("resultado"); document.getElementById("M_C").innerHTML = "Massa por Ciclo:" + Massa_Ciclo +" Ton/FP";
+  }
